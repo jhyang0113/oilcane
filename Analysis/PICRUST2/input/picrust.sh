a35b59f1513f885be8de32ca3071f71885bfa39b
jhyang0113 <49726623+jhyang0113@users.noreply.github.com>
2023-04-19 12:18:18 -0500

    Delete picrust.sh

--- Analysis/PICRUST2/input/picrust.sh
+++
@@ -1,5 +0,0 @@
-/picrust2-2.5.0/scripts/picrust2_pipeline.py -s core_total_leaf_g_ssu_picrust.fasta -i input_leaf.txt -o picrust2_out_pipeline_leaf -p 10 --per_sequence_contrib
-/picrust2-2.5.0/scripts/picrust2_pipeline.py -s core_total_stem_g_ssu_picrust.fasta -i input_stem.txt -o picrust2_out_pipeline_stem -p 10 --per_sequence_contrib
-/picrust2-2.5.0/scripts/picrust2_pipeline.py -s core_total_root_g_ssu_picrust.fasta -i input_root.txt -o picrust2_out_pipeline_root -p 10 --per_sequence_contrib
-/picrust2-2.5.0/scripts/picrust2_pipeline.py -s core_total_rootsoil_g_ssu_picrust.fasta -i input_rootsoil.txt -o picrust2_out_pipeline_rootsoil -p 10 --per_sequence_contrib
-/picrust2-2.5.0/scripts/picrust2_pipeline.py -s core_total_bulksoil_g_ssu_picrust.fasta -i input_bulksoil.txt -o picrust2_out_pipeline_bulksoil -p 10 --per_sequence_contrib
